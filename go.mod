fe79c11175415b789176adfd6c10f00a0b81c2a2
Arpit Shukla <shills.arpit68@gmail.com>
2023-06-25 16:36:31 +0530

    Completed testing AI-BOT

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/Krognol/go-wolfram v0.0.0-20180610151123-5b91101b92a8 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/robfig/cron v1.2.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
@@ -15,5 +16,8 @@ require (
 	github.com/shomali11/proper v0.0.0-20190608032528-6e70a05688e7 // indirect
 	github.com/shomali11/slacker v1.4.1 // indirect
 	github.com/slack-go/slack v0.12.2 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/wit-ai/wit-go/v2 v2.0.2 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/Krognol/go-wolfram"
 	"github.com/joho/godotenv"
 	"github.com/shomali11/slacker"
+	"github.com/tidwall/gjson"
 	witai "github.com/wit-ai/wit-go/v2"
 )
 
@@ -16,26 +19,35 @@ func main() {
 	APP_TOKEN := os.Getenv("APP_TOKEN")
 	BOT_TOKEN := os.Getenv("BOT_TOKEN")
 	WIT_AI_TOKEN := os.Getenv("WIT_AI_TOKEN")
+	WOLFRAM_APP_ID := os.Getenv("WOLFRAM_APP_ID")
 
 	bot := slacker.NewClient(BOT_TOKEN, APP_TOKEN)
 
 	client := witai.NewClient(WIT_AI_TOKEN)
 
-	bot.Command("<sentence>", &slacker.CommandDefinition{
-		Description: "Say a sentence!",
-		Examples:    []string{"say hello there everyone!"},
+	c := &wolfram.Client{AppID: WOLFRAM_APP_ID}
+
+	bot.Command("query <sentence>", &slacker.CommandDefinition{
+		Description: "Ask a question!",
+		Examples:    []string{"Who is the prime minister of INDIA?"},
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			sentence := request.Param("sentence")
-			fmt.Println(sentence)
 			msg, _ := client.Parse(&witai.MessageRequest{
 				Query: sentence,
 			})
-			fmt.Printf("%v", msg.Entities)
-			fmt.Printf("%v", msg.ID)
-			fmt.Printf("%v", msg.Intents)
-			fmt.Printf("%v", msg.Text)
-			fmt.Printf("%v", msg.Traits)
-			response.Reply(sentence)
+			data, _ := json.MarshalIndent(msg, "", "    ")
+			rough := string(data[:])
+			fmt.Println(rough)
+			val := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query.0.value")
+			query := val.String()
+			fmt.Println(query)
+
+			res, err := c.GetSpokentAnswerQuery(query, wolfram.Metric, 1000)
+			if err != nil {
+				log.Fatal("Error occured while using wolfram api :", err)
+			}
+			fmt.Println(res)
+			response.Reply(res)
 		},
 	})
 

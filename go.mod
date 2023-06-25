abe39f0657767a9a824290688a47defa20c682de
Arpit Shukla <shills.arpit68@gmail.com>
2023-06-25 14:29:12 +0530

    Completed slack setup'

--- go.mod
+++ go.mod
@@ -2,4 +2,18 @@ module github.com/12A-r-p-i-t/AI-BOT
 
 go 1.19
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/shomali11/slacker/v2 v2.0.0-alpha1
+)
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/robfig/cron v1.2.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/shomali11/commander v0.0.0-20220716022157-b5248c76541a // indirect
+	github.com/shomali11/proper v0.0.0-20190608032528-6e70a05688e7 // indirect
+	github.com/shomali11/slacker v1.4.1 // indirect
+	github.com/slack-go/slack v0.12.2 // indirect
+	github.com/wit-ai/wit-go/v2 v2.0.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/shomali11/slacker"
+	witai "github.com/wit-ai/wit-go/v2"
 )
 
 func main() {
 	godotenv.Load()
 	APP_TOKEN := os.Getenv("APP_TOKEN")
 	BOT_TOKEN := os.Getenv("BOT_TOKEN")
+	WIT_AI_TOKEN := os.Getenv("WIT_AI_TOKEN")
+
+	bot := slacker.NewClient(BOT_TOKEN, APP_TOKEN)
+
+	client := witai.NewClient(WIT_AI_TOKEN)
+
+	bot.Command("<sentence>", &slacker.CommandDefinition{
+		Description: "Say a sentence!",
+		Examples:    []string{"say hello there everyone!"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			sentence := request.Param("sentence")
+			fmt.Println(sentence)
+			msg, _ := client.Parse(&witai.MessageRequest{
+				Query: sentence,
+			})
+			fmt.Printf("%v", msg.Entities)
+			fmt.Printf("%v", msg.ID)
+			fmt.Printf("%v", msg.Intents)
+			fmt.Printf("%v", msg.Text)
+			fmt.Printf("%v", msg.Traits)
+			response.Reply(sentence)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
